c90b328bc4bd94f769ce0e31a4625cfa2a5fa928
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-01 13:47:15 -0800

    added auth

--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
 const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
+const { getUserId } = require('./utils');
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const fs = require('fs');
 const path = require('path');
 const prisma = new PrismaClient();
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany();
-    },
-  },
-  Mutation: {
-    post: async (parent, args, context, info) => {
-      const newLink = await context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      });
-      return newLink;
-    },
-  },
-};
+    Query,
+    Mutation,
+    User,
+    Link
+}
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(
@@ -31,9 +24,16 @@ const server = new ApolloServer({
     'utf8'
   ),
   resolvers,
-  context: {
-    prisma,
-  },
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  }
 });
 
 server
