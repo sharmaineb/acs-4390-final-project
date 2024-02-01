b61f166a45e39f00d9caf63196bbfe79de447d5d
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-01 14:03:04 -0800

    added subscriptions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
+
+const { PubSub } = require('apollo-server')
+
+const pubsub = new PubSub()
 
 const fs = require('fs');
 const path = require('path');
@@ -14,27 +20,30 @@ const prisma = new PrismaClient();
 const resolvers = {
     Query,
     Mutation,
+    Subscription,
     User,
-    Link
+    Link,
+    Vote,
 }
 
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(
-    path.join(__dirname, 'schema.graphql'),
-    'utf8'
-  ),
-  resolvers,
-  context: ({ req }) => {
-    return {
-      ...req,
-      prisma,
-      userId:
-        req && req.headers.authorization
-          ? getUserId(req)
-          : null
-    };
-  }
-});
+    typeDefs: fs.readFileSync(
+      path.join(__dirname, 'schema.graphql'),
+      'utf8'
+    ),
+    resolvers,
+    context: ({ req }) => {
+      return {
+        ...req,
+        prisma,
+        pubsub,
+        userId:
+          req && req.headers.authorization
+            ? getUserId(req)
+            : null
+      };
+    }
+  });
 
 server
   .listen()
